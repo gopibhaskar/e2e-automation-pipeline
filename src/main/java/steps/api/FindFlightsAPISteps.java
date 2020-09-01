822fbc290aea019a46c487fe986dff9f36727b79
Papanaboina, Gopibhaskar <Gopibhaskar.Papanaboina@sabre.com>
2020-09-01 15:52:21 +0530

    For first build

--- src/main/java/steps/api/FindFlightsAPISteps.java
+++ src/main/java/steps/api/FindFlightsAPISteps.java
@@ -7,6 +7,6 @@ import static net.serenitybdd.rest.SerenityRest.given;
 public class FindFlightsAPISteps {
     @Step
     public void findFlights() {
-        given().parameters("fromPort", "Paris", "toPort", "Buenos Aires").when().post("http://testdemo.com/reserve.php");
+        given().parameters("fromPort", "Paris", "toPort", "Buenos Aires").when().post("http://blazedemo.com/reserve.php");
     }
 }
