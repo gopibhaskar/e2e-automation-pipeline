392e8b88e769b66a8df8a2e75f6c23d9c9cafb1a
gopibhaskar <gopibhaskar1993@gmail.com>
2020-09-03 13:19:04 +0530

    Update E2E-automation-test.groovy

--- jenkins-pipelines/E2E-automation-test.groovy
+++ jenkins-pipelines/E2E-automation-test.groovy
@@ -28,6 +28,19 @@ node {
                     reportName: "API tests report"
             ])
         }
+    }    
+    stage('UI') {
+        try {
+            sh "mvn clean verify -Dtags='type:UI'"
+        } catch (err) {
+
+        } finally {
+            publishHTML (target: [
+                    reportDir: 'target/site/serenity',
+                    reportFiles: 'index.html',
+                    reportName: "UI tests report"
+            ])
+        }
     }
     stage('Results') {
         junit '**/target/failsafe-reports/*.xml'
